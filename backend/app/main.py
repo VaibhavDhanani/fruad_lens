74cf709a4568eb61f4b7b6a2f399fe294b6f0a1d
YashGabani845 <gabaniyash845@gmail.com>
2025-05-06 11:26:18 +0530

    model setup added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Security
 from app.api.dependencies import get_current_user
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.models.models import User
 import uvicorn
 from app.api.routes import (
@@ -11,11 +13,18 @@ from app.api.routes import (
     risk_assessment_routes,
     transaction_metrics_routes,
     fraud_details_routes,
-    auth_routes
+    auth_routes,
+    model_routes
 )
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace with ["http://localhost:5173"] in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(user_routes.router)
 app.include_router(account_routes.router)
 app.include_router(device_routes.router)
@@ -25,6 +34,7 @@ app.include_router(risk_assessment_routes.router)
 app.include_router(fraud_details_routes.router)
 app.include_router(transaction_metrics_routes.router)
 app.include_router(auth_routes.router)
+app.include_router(model_routes.router)
 
 
 @app.get("/", tags=["Status"])
